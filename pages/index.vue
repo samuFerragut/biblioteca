84cc2d52af7e05030fad494e8203e15bbc5d13a4
samuFerragut <samufuerza@gmail.com>
2022-05-18 19:26:17 +0200

    feat: merge branche

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <v-card>
-        drghaer
-      </v-card>
-    </v-layout>
-  </v-container>
-</template>
+    <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card flat class="ma-3 text-xs-center">
+        <v-card-title primary-title>
+          <div class="justify-center">
+            <h3 class="headline mb-0">Biblioteca Jovent</h3>
+          </div>
+        </v-card-title>
 
+        <v-card-actions>
+          <v-btn flat color="orange">Inserta aquí un indice</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+ai
 <script>
 export default {
   name:'BibliotecaJovent',
